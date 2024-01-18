901540f14a8ea7213c2930b288031911887aabdd
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-01-19 01:49:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 class Config:
     API_ID = "29670093"
     API_HASH = "35e5f212813cbb3e6401157ae2256f31"
-    TOKEN = "6406896517:AAFxfNo29sIX3J3PxTxytvtaRlQaw99nNZU"
+    TOKEN = "6830418705:AAH8RBioB6djGeZxwaiyUIxDEx2V96ocpfk"
     MONGO_URL = "mongodb+srv://RUDRA_JAAT1:RUDRAJAAT@rudrajaat.zbir7lx.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/34cf58490b86db4d4fba5.jpg"
     SUDOERS = filters.user(["6134246701"])
