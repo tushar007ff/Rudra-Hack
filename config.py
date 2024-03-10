7d793aeca97aedbc0c7fc1655b0b2100a05cf6f6
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-03-10 23:20:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 class Config:
     API_ID = "29670093"
     API_HASH = "35e5f212813cbb3e6401157ae2256f31"
-    TOKEN = "6920305366:AAFEMMrpSq-OJo1XCFj3ozBrz4gpEpFJP-c"
+    TOKEN = "6402382723:AAGoisPHJrtuKJEWEKHgEbV45UGmXAh2dUg"
     MONGO_URL = "mongodb+srv://RUDRA_JAAT1:RUDRAJAAT@rudrajaat.zbir7lx.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/34cf58490b86db4d4fba5.jpg"
     SUDOERS = filters.user(["1826484283"])
