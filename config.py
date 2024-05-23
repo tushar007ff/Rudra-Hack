06154d56c79ed02f75e7ff2f165e090bb1e97a1b
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-05-23 22:35:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
 class Config:
-    API_ID = "29670093"
-    API_HASH = "35e5f212813cbb3e6401157ae2256f31"
-    TOKEN = "6402382723:AAGoisPHJrtuKJEWEKHgEbV45UGmXAh2dUg"
+    API_ID = "28509005"
+    API_HASH = "dc43ab3927d0c4da0067ee3100ea2f08"
+    TOKEN = "6920305366:AAFEMMrpSq-OJo1XCFj3ozBrz4gpEpFJP-c"
     MONGO_URL = "mongodb+srv://RUDRA_JAAT1:RUDRAJAAT@rudrajaat.zbir7lx.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/34cf58490b86db4d4fba5.jpg"
     SUDOERS = filters.user(["1826484283"])
